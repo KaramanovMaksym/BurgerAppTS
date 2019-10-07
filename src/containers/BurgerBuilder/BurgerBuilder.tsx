870403cbd7a51d99e9e66fd40d34079d3b4bf4ac
Maksym Karamanov <karamanov.maksym@ukr.net>
2019-10-07 12:52:58 +0300

    Save orders on Firebase

--- src/containers/BurgerBuilder/BurgerBuilder.tsx
+++ src/containers/BurgerBuilder/BurgerBuilder.tsx
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 import Modal from '../../components/UI/Modal/Modal'
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
+import axios from '../../axios-orders'
 
 interface Props {
 
@@ -126,6 +127,23 @@ export default class BurgerBuilder extends Component<Props, State> {
   }
 
   purchaseContinueHandler = () => {
-    alert('You continue!')
+    // alert('You continue!')
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice.toFixed(2),
+      customer: {
+        name: 'Max',
+        address: {
+          street: 'Teststreet 1',
+          zipCode: '69096',
+          country: 'Ukraine'
+        },
+        email: 'test@test.com'
+      },
+      deliveryMethod: 'fastest'
+    }
+    axios.post('/orders.json', order)
+      .then(resp => console.log(resp))
+      .catch(err => console.log(err))
   }
 }
